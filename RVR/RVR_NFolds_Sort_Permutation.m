c50348300b3d6d1b1ec7e69f1b0a72907bd4f06b
Zaixu Cui <zaixucui@ZaixudeMBP.fios-router.home>
2018-10-20 14:16:42 -0400

    code

--- RVR/RVR_NFolds_Sort_Permutation.m
+++ RVR/RVR_NFolds_Sort_Permutation.m
@@ -1,12 +1,14 @@
 
-function RVR_NFolds_Sort_Permutation(Subjects_Data_Path, Subjects_Scores, Times, FoldQuantity, ResultantFolder)
+function RVR_NFolds_Sort_Permutation(Subjects_Data, Subjects_Scores, Times, FoldQuantity, Pre_Method, ResultantFolder)
 %
 % Doing permutation test to create the distribution of random model.
 % Because permutation is pretty slow, here we parallized the jobs using
 % PSOM (http://psom.simexp-lab.org/).
 %
-% Subjects_Data_Path:
-%         The path of the .mat file that storing the m*n matrix variable 
+% Subjects_Data:
+%           m*n matrix
+%           m is the number of subjects
+%           n is the number of features
 %
 % Subjects_Scores:
 %         m*1 vector, the scores of all subjects
@@ -16,6 +18,9 @@ function RVR_NFolds_Sort_Permutation(Subjects_Data_Path, Subjects_Scores, Times,
 % 
 % FoldQuantity:
 %         Quantity of the cross validation, 5 or 10 is recommended
+%
+% Pre_Method:
+%           'Normalize', 'Scale', 'None'
 %
 % ResultantFolder:
 %         Path of the folder for stroing the results.
@@ -37,25 +42,8 @@ for i = 1:Times
     ResultantFolder_I = [ResultantFolder filesep 'Time_' num2str(i)];
     mkdir(ResultantFolder_I);
     
-    Job_Name = ['perm_' num2str(i)];
-    pipeline.(Job_Name).command = 'RVR_NFolds_Sort_Permutation_Sub(opt.para1, opt.para2, '''', opt.para3, ''Scale'', opt.para4)';
-    pipeline.(Job_Name).opt.para1 = Subjects_Data_Path;
-    pipeline.(Job_Name).opt.para2 = Subjects_Scores;
-    pipeline.(Job_Name).opt.para3 = FoldQuantity;
-    pipeline.(Job_Name).opt.para4 = ResultantFolder_I;
-
+    RVR_NFolds_Sort(Subjects_Data, Subjects_Scores, FoldQuantity, Pre_Method, 0, 1, ResultantFolder)
 end
 
-psom_gb_vars;
-Pipeline_opt.mode = 'batch'; % 'batch' means parallized within a single computer; If parallizing on a SGE cluster, using 'qsub'
-Pipeline_opt.qsub_options = '-q veryshort.q'; % if 'mode' is 'qsub', here specifying the queue name
-Pipeline_opt.mode_pipeline_manager = 'batch';
-Pipeline_opt.max_queued = 40;
-Pipeline_opt.flag_verbose = 1;
-Pipeline_opt.flag_pause = 0;
-Pipeline_opt.flag_update = 1;
-Pipeline_opt.path_logs = [ResultantFolder filesep 'logs'];
-
-psom_run_pipeline(pipeline,Pipeline_opt);
 
 
