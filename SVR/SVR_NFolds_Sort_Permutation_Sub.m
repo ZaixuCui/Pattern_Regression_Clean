3df0745db1b45ffb5e909653d3e38a5c8d368a51
Zaixu Cui <ZaixuCui@users.noreply.github.com>
2018-10-21 16:51:22 -0400

    Update SVR_NFolds_Sort_Permutation_Sub.m

--- SVR/SVR_NFolds_Sort_Permutation_Sub.m
+++ SVR/SVR_NFolds_Sort_Permutation_Sub.m
@@ -3,6 +3,18 @@ function SVR_NFolds_Sort_Permutation_Sub(Subjects_Data_Path, Subjects_Scores, Fo
 %
 % Sub-function for the SVR_NFolds_Sort_Permutation.m
 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Written by Zaixu Cui: zaixucui@gmail.com;
+%                       Zaixu.Cui@pennmedicine.upenn.edu
+%
+% If you use this code, please cite: 
+%                       Cui et al., 2018, Cerebral Cortex; 
+%                       Cui and Gong, 2018, NeuroImage; 
+%                       Cui et al., 2016, Human Brain Mapping.
+% (google scholar: https://scholar.google.com.hk/citations?user=j7amdXoAAAAJ&hl=zh-TW&oi=ao)
+%
+
 tmp = load(Subjects_Data_Path);
 FieldName = fieldnames(tmp);
 Subjects_Data = tmp.(FieldName{1});
