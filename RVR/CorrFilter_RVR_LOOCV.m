0359ddfec57a78b2ffca2fb6c9c08d977ad74b8e
Zaixu Cui <ZaixuCui@users.noreply.github.com>
2018-10-21 16:46:15 -0400

    Update CorrFilter_RVR_LOOCV.m

--- RVR/CorrFilter_RVR_LOOCV.m
+++ RVR/CorrFilter_RVR_LOOCV.m
@@ -28,7 +28,7 @@ function Prediction = CorrFilter_RVR_LOOCV(Subjects_Data, Subjects_Scores, CoefT
 %
 % If you use this code, please cite: 
 %                       Cui et al., 2018, Cerebral Cortex; 
-%                       Cui and Gong et al., 2018, NeuroImage; 
+%                       Cui and Gong, 2018, NeuroImage; 
 %                       Cui et al., 2016, Human Brain Mapping.
 % (google scholar: https://scholar.google.com.hk/citations?user=j7amdXoAAAAJ&hl=zh-TW&oi=ao)
 %
